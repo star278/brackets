d1659983076e54a86f534d7c3fbb4096efac6af0
Ilya Ivashko <ivashko-ilya@mail.ru>
2020-03-22 23:34:09 +0300

    feat: implemented function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var bracketsSet = getBracketsSets(bracketsConfig);
+
+    for (let i = 0; i < bracketsSet.length;) {
+        if (str.indexOf(bracketsSet[i]) !== -1) {
+            str = str.replace(bracketsSet[i], '');
+            i = 0;
+        } else i++;
+    }
+    return !str;
+}
+
+function getBracketsSets(config) {
+    return config.map(function(item) {
+        return item.join('');
+    });
 }
